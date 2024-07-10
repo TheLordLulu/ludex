fe3649987e13f8e8aec638a2d2c1962581f28a72
Lusine Kenneth <lusinekenneth2@gmail.com>
2024-07-09 21:21:47 -0400

    added sorting to the pokedex

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { PokemonTypes } from "../PokemonTypes/PokemonTypes";
+
+
 
 interface Stat {
   base_stat: number;
@@ -18,17 +20,21 @@ interface Pokemon {
   stats: Stat[];
 }
 
-
-
 export const Pokedex = () => {
   const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
+  const [sortField, setSortField] = React.useState<string | null>("");
+  const [sortOrder, setSortOrder] = React.useState<"asc" | "desc">("asc");
 
+
+  // fetch data from the pokeapi
   useEffect(() => {
     const fetchPokemon = async () => {
       const response = await fetch(
         "https://pokeapi.co/api/v2/pokemon?limit=151"
       );
       const data = await response.json();
+
+      // fetch detailed data for each pokemon
       const detailedPokemonData = await Promise.all(
         data.results.map(async (pokemon) => {
           const response = await fetch(pokemon.url);
@@ -44,33 +50,77 @@ export const Pokedex = () => {
     fetchPokemon();
   }, []);
 
+
+  // sort the pokemons based on the sortField and sortOrder
+  const getSortedPokemons = () => {
+    return pokemons.slice().sort((a, b) => {
+      if (sortField === "name") {
+        // for string fields, we need to convert them to lowercase
+        const fieldA = a[sortField].toString().toLowerCase();
+        const fieldB = b[sortField].toString().toLowerCase();
+        if (fieldA < fieldB) return sortOrder === 'asc' ? -1 : 1;
+        if (fieldA > fieldB) return sortOrder === 'asc' ? 1 : -1;
+        return 0;
+      } else if (sortField === "id") {
+        // Treat id as numeric value
+        const idA = parseInt(a[sortField].toString());
+        const idB = parseInt(b[sortField].toString());
+        return sortOrder === 'asc' ? idA - idB : idB - idA;
+      } else if (sortField === "total") {
+        // for total, we need to sum all stats
+        const totalA = a.stats.reduce((total, stat) => total + stat.base_stat, 0);
+        const totalB = b.stats.reduce((total, stat) => total + stat.base_stat, 0);
+        return sortOrder === 'asc' ? totalA - totalB : totalB - totalA;
+      } else {
+        // for other stats, we need to find the stat by name
+        const statA = a.stats.find(stat => stat.stat.name === sortField)?.base_stat || 0;
+        const statB = b.stats.find(stat => stat.stat.name === sortField)?.base_stat || 0;
+        return sortOrder === 'asc' ? statA - statB : statB - statA;
+      }
+    });
+  };
+
+  // handle sorting when a column is clicked
+  const handleSort = (field: string) => {
+    if (sortField === field) { //
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else { // if the field is different, we set the field and order to default
+      setSortField(field);
+      setSortOrder("asc");
+    }
+  }
+
   return (
     <div className="bg-white">
       <table className="mx-auto">
         <thead className="text-black bg-[#747474]">
           <tr className="">
-            <th className="p-2">Sprite</th> 
-            <th className="p-2">Number</th>
-            <th className="p-2">Name</th>
+            <th className="p-2">Sprite</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('id')}>Number</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('name')}>Name</th>
             <th className="p-2">Type</th>
-            <th className="p-2">Total</th>
-            <th className="p-2">HP</th>
-            <th className="p-2">Attack</th>
-            <th className="p-2">Defense</th>
-            <th className="p-2">Sp.Atk</th>
-            <th className="p-2">Sp.Def</th>
-            <th className="p-2">Speed</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('total')}>Total</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('hp')}>HP</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('attack')}>Attack</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('defense')}>Defense</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('special-attack')}>Sp.Atk</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('special-defense')}>Sp.Def</th>
+            <th className="p-2 cursor-pointer" onClick={() => handleSort('speed')}>Speed</th>
           </tr>
         </thead>
         <tbody>
-          {pokemons.map((pokemon) => (
+          {getSortedPokemons().map((pokemon) => (
             <tr key={pokemon.id}>
               <td>
                 <img src={pokemon.sprites.front_default} alt={pokemon.name} />
               </td>
               <td className="text-gray-700">{pokemon.id}</td>
-              <td className="text-blue-500 capitalize font-bold ">{pokemon.name}</td>
-              <td className="uppercase font-semibold"><PokemonTypes types={pokemon.types} /></td>
+              <td className="text-blue-500 capitalize font-bold ">
+                {pokemon.name}
+              </td>
+              <td className="uppercase font-semibold">
+                <PokemonTypes types={pokemon.types} />
+              </td>
               <td className="text-black font-semibold">
                 {pokemon.stats.reduce(
                   (total, stat) => total + stat.base_stat,
@@ -78,7 +128,9 @@ export const Pokedex = () => {
                 )}
               </td>
               {pokemon.stats.map((stat) => (
-                <td className="text-gray-500" key={stat.stat.name}>{stat.base_stat}</td>
+                <td className="text-gray-500" key={stat.stat.name}>
+                  {stat.base_stat}
+                </td>
               ))}
             </tr>
           ))}
